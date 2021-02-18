9031e542630fe43db8cd52441c8a2fd034ca8c80
Lucas Cardozo <ishida393@gmail.com>
2021-02-18 00:02:46 -0300

    volvimos al pasado

--- vulkan/controllers/productsController.js
+++ vulkan/controllers/productsController.js
@@ -1,84 +1,95 @@
 const fs = require("fs");
-const products = JSON.parse(fs.readFileSync("./data/products.json", "utf-8"));
+const products = JSON.parse(fs.readFileSync("./data/products.json","utf-8"));
 const path = require("path");
-module.exports = {
+module.exports =  {
     //aca empieza la muestra de productos de diferentes categorias
-    mostrar: (req, res) => {
-        res.render("products", { css: '/stylesheets/products.css', products });
+    mostrar: (req,res) =>{  
+        res.render("products",{css:'/stylesheets/products.css',products});
+    },   
+    showCategoryFuentes: (req,res)=>{
+        res.render("fuentes",{css:"/stylesheets/productsCategory.css",products});
     },
-    showCategoryFuentes: (req, res) => {
-        res.render("fuentes", { css: "/stylesheets/productsCategory.css", products });
+    showCategoryGabinetes: (req,res)=>{
+        
+        res.render("gabinetes",{css:"/stylesheets/productsCategory.css",products});
     },
-    showCategoryGabinetes: (req, res) => {
-
-        res.render("gabinetes", { css: "/stylesheets/productsCategory.css", products });
+    showCategoryMotherboards: (req,res)=>{
+        res.render("motherboard",{css:"/stylesheets/productsCategory.css",products});
     },
-    showCategoryMotherboards: (req, res) => {
-        res.render("motherboard", { css: "/stylesheets/productsCategory.css", products });
+    showCategoryMouses: (req,res)=>{
+        res.render("mouse",{css:"/stylesheets/productsCategory.css",products});
     },
-    showCategoryMouses: (req, res) => {
-        res.render("mouse", { css: "/stylesheets/productsCategory.css", products });
+    showCategoryPlacas: (req,res)=>{
+        res.render("placa",{css:"/stylesheets/productsCategory.css",products});
     },
-    showCategoryPlacas: (req, res) => {
-        res.render("placa", { css: "/stylesheets/productsCategory.css", products });
+    showCategoryProcesadores: (req,res)=>{
+        res.render("procesador",{css:"/stylesheets/productsCategory.css",products});
     },
-    showCategoryProcesadores: (req, res) => {
-        res.render("procesador", { css: "/stylesheets/productsCategory.css", products });
+    showCategoryMemorias: (req,res)=>{
+        res.render("memorias",{css:"/stylesheets/productsCategory.css",products});
     },
-    showCategoryMemorias: (req, res) => {
-        res.render("memorias", { css: "/stylesheets/productsCategory.css", products });
+    showCategoryTeclados: (req,res)=>{
+        res.render("teclados",{css:"/stylesheets/productsCategory.css",products});
     },
-    showCategoryTeclados: (req, res) => {
-        res.render("teclados", { css: "/stylesheets/productsCategory.css", products });
+    showCategoryCoolers: (req,res)=>{
+        res.render("coolers",{css:"/stylesheets/productsCategory.css",products});
     },
-    showCategoryCoolers: (req, res) => {
-        res.render("coolers", { css: "/stylesheets/productsCategory.css", products });
-    },
-    storeProduct: (req, res) => {
+    storeProduct: (req,res)=>{
         console.log(req.body)
-        const { name, category, brand, price, description } = req.body
+        const {name,category,brand,price,description} = req.body
         let lastId = 0;
         products.forEach(element => {
-            if (element.id == id) {
-                id = id
+            if(element.id > lastId){
+               lastId = element.id
             }
         });
         const product = {
-            id: id + 1,
+            id : lastId +1,
             name,
             category,
             brand,
             price,
+            description,
             image: req.files[0].filename
         }
-        products.push(product);
-        let nuevojson = JSON.stringify(products);
-        fs.writeFileSync("./data/products.json", nuevojson, "utf-8");
-        console.log(product);
-        res.redirect("/products/create");
-
+         products.push(product);
+         let nuevojson = JSON.stringify(products);
+         fs.writeFileSync("./data/products.json",nuevojson,"utf-8");
+         res.redirect("/products/create");
+        
     },
-    productDetail: (req, res) => {
+    cart : (req,res) =>{  
+        res.render("carrito",{css:'/stylesheets/carrito.css'});
+    },
+    productDetail: (req,res)=>{
         const id = req.params.id
         let product;
         products.forEach(element => {
-            if (element.id == id) {
+            if(element.id == id){
                 product = element
             }
         });
-        res.render("detail", { css: '/stylesheets/index.css', product });
+        res.render("detail",{css:'/stylesheets/index.css',product});
     },
-    productDetail: (req, res) => {
-        res.render("detail", { css: '/stylesheets/index.css' });
+    crearProducto : (req,res)=>{
+        res.render("productCreate",{css:'/stylesheets/admin.css'});
     },
-    crearProducto: (req, res) => {
-        res.render("productCreate", { css: '/stylesheets/admin.css' });
-    },
-    edit: (req, res) => {
-        let id = req.params.id
-        const { name, category, brand, price, description } = req.body
+    showEdit: (req,res)=>{
+        const id = req.params.id
+        let product;
         products.forEach(element => {
-            if (element.id == Number(id)) {
+            if(element.id == id){
+                product = element
+            }
+        });
+
+        res.render("productEdit",{css:'/stylesheets/admin.css', product });
+    },
+    edit: (req,res)=>{
+        let id = req.params.id
+        const {name,category,brand,price,description} = req.body
+        products.forEach(element =>{
+            if(element.id == Number(id)){
                 element.name = name;
                 element.category = category;
                 element.brand = brand;
@@ -87,39 +98,26 @@ module.exports = {
             }
         });
         let nuevijson = JSON.stringify(products);
-        fs.writeFileSync("./data/products.json", nuevijson, "utf-8");
-        console.log(id)
-        res.redirect("/products");
+         fs.writeFileSync("./data/products.json",nuevijson,"utf-8");
+           console.log(id)
+         res.redirect("/products");
     },
-    delete: (req, res) => {
+    delete: (req,res)=>{
         let id = req.params.id
         let indice;
         products.forEach(element => {
-            if (element.id == Number(id)) {
-                indice = products.indexOf(element)
-                if (fs.existsSync(path.join('public', 'images', element.image))) {
-                    fs.unlinkSync(path.join('public', 'images', element.image))
-                }
+            if(element.id == Number(id)){
+             indice = products.indexOf(element)
+             if(fs.existsSync(path.join('public','images',element.image))){
+                fs.unlinkSync(path.join('public','images',element.image))
+            }
             }
         });
-
-        products.splice(indice, 1);
+        
+        products.splice(indice,1);
         let nuevijson = JSON.stringify(products);
-        fs.writeFileSync("./data/products.json", nuevijson, "utf-8");
-        console.log(id)
-        res.redirect("/");
-    },
-    showEdit: (req, res) => { 
-        const id = req.params.id
-        let product;
-        products.forEach(element => {
-            if(element.id == id){
-                product = element
-            }
-        });
-        res.render("productEdit", { css: '/stylesheets/admin.css' });
-    },
-    cart : (req, res) =>{
-        res.render("carrito", { css: '/stylesheets/carrito.css'});
+         fs.writeFileSync("./data/products.json",nuevijson,"utf-8");
+           console.log(id)
+         res.redirect("/");
     }
 }
--- vulkan/middlewares/multer.js
+++ vulkan/middlewares/multer.js
@@ -5,11 +5,11 @@ const storage = multer.diskStorage({
     destination: (req,file,cb)=> {
         cb(null,"public/images")
     },
-    filename: (req, file, cb) => {
-        cb(null, file.fieldname + "-" + Date.now() + path.extname(file.originalname))
+    filename : (req,file,cb)=>{
+        cb(null, file.fieldname + "-" + Date.now() + path.extname(file.originalname) )
     }
 });
 
-const upload = multer({ storage: storage });
+const upload = multer({storage: storage});
 
 module.exports = upload;
